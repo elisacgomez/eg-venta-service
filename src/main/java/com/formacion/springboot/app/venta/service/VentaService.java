87288fd7a13cc517c889b10a7ef8df06d4f608ef
Elisa <egomezvivas@fusap.com.ar>
2020-07-08 17:13:02 -0300

    CRUD Incluyendo metodos para crear/eliminar y actualizar el producto libro desde el microservicio cliente venta.

--- src/main/java/com/formacion/springboot/app/venta/service/VentaService.java
+++ src/main/java/com/formacion/springboot/app/venta/service/VentaService.java
@@ -2,10 +2,18 @@ package com.formacion.springboot.app.venta.service;
 
 import java.util.List;
 
+import com.formacion.springboot.app.venta.models.Libro;
 import com.formacion.springboot.app.venta.models.Venta;
 
 public interface VentaService {
 
 	public List<Venta> findAll();
 	public Venta findById(Long id, Integer cantidad);
+	
+	public Libro save(Libro libro);
+	
+	public Libro update(Libro libro, Long id);
+	
+	public void delete(Long id);
+	
 }
